c36c24e92c80055217058b1dd585fbb2de8d6fbd
Casstiel <cpss5433@gmail.com>
2022-12-22 15:06:39 -0800

    search bar done

--- react-app/src/components/navigation/searchBar.js
+++ react-app/src/components/navigation/searchBar.js
@@ -14,18 +14,65 @@ const SearchBar = () => {
         dispatch(videoActions.fetchAllVideos())
     }, [])
 
+    const handleFilter = (e) => {
+        const searchWord = e.target.value
+        console.log('rrrrrrrrrrrrrrrr',  typeof searchWord)
+        setWordEntered(searchWord)
+        const newFilter = videos.filter((video)=> {
+            // console.log(video.title.toUpperCase().split(' '))
+            // console.log(searchWord.toUpperCase())
+            console.log(video.title.toLowerCase().split(' ').includes(searchWord.toLowerCase()))
+            return video.title.toLowerCase().split(' ').includes(searchWord.toLowerCase())
+        })
+        if(searchWord === ''){
+            setFilteredData([])
+        } else {
+            setFilteredData(newFilter)
+            console.log('fffffffffffffffff', filteredData)
+        }
+    }
+
+    const clearInput = () => {
+        setFilteredData([]);
+        setWordEntered("");
+      };
+
     return (
+        <div>
         <div className='search-wrapper'>
          <div>
             <input
             name='search'
             type='text'
-            placeholder='search'
+            placeholder='Search'
             className='searchInputs'
+            value={wordEntered}
+            onChange={handleFilter}
             />
         </div>
         <div className='search-button'>
             <img id='search-icon' src={search}></img>
+        </div>
+        </div>
+        <div className='searchall'>
+            {filteredData.length !== 0 && (
+                <div className='data-result'>
+                    {filteredData.map((value)=>{
+                        return <div className='res-row'>
+                                <NavLink className='data-item'
+                                key={value.id}
+                                to={`videos/${value.id}`}
+                                onClick={clearInput}
+                                >
+                                    <div id='sb-space'></div>
+                         <img id='search-res-pic' src={search}></img>
+                         <p id='search-res-text'>{value.title}</p>
+                        </NavLink>
+                            </div>
+                    })
+                    }
+                </div>
+            )}
         </div>
         </div>
     )
